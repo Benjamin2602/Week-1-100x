247106c179df89aa392718cd237098ed0ee952d3
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-08-18 11:25:35 +0530

    anagram done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,27 @@
 */
 
 function isAnagram(str1, str2) {
+  let len1 = str1.length;
+  let len2 = str2.length;
+  //checking if the length of the strings are equal
+  if (len1 != len2) {
+    return false;
+  }
+  //converting the string into an array
+  //.sort used to sort the individual elements of the array
+  let str1Arr = str1.split("").sort();
+  let str2Arr = str2.split("").sort();
 
+  //converting the array back to string
+  let arr1Str = str2Arr.join("");
+  let arr2Str = str1Arr.join("");
+
+  if (arr1Str === arr2Str) {
+    console.log("True , It is an anagram");
+  } else {
+    console.log("false, it is not an anagram");
+  }
 }
+isAnagram("spar", "rasp");
 
 module.exports = isAnagram;
