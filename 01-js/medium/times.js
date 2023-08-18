5996c2def85a42be6e018107b10606e32bdb085d
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-08-18 16:59:03 +0530

    time

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,16 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+    const startTime = new Date().getTime();
+ 
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    const endTime = new Date().getTime();
+    const sec = (endTime - startTime)/1000;
+    return sec;
 }
+
+var answer = calculateTime(100)
+console.log(answer)
